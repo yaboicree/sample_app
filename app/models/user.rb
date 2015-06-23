ff5ac77a3926832f9b72230f356833f9828312eb
Chris <andersan@umich.edu>
2015-06-23 13:57:21 +0100

    finish log in and logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
 	before_save { self.email = email.downcase}
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -13,5 +14,27 @@ class User < ActiveRecord::Base
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 																									BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
+	end
+
+	#returns a random token (initially made for remembering users)
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	# remembers a user in database to use in "persistent sessions"
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	# returns true if token matches digest --- digest = secure bcrypt generated version of password 
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	#forget a user - long-term cookies..
+	def forget
+		update_attribute(:remember_digest, nil)
 	end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,24 @@ class ActiveSupport::TestCase
   def is_logged_in?
   	!session[:user_id].nil?
   end
+
+  # log in as test user... for use by tests (duh)
+  def log_in_as(user, options ={})
+  	password 		= options[:password] 		|| 'password'
+  	remember_me = options[:remember_me] || '1' 
+  	if integration_test?
+  		post login_path, session: { email: 			 user.email,
+  																password: 	 password,
+																	remember_me: remember_me}
+		else
+			session[:user_id] = user.id
+		end
+	end
+
+	private
+
+		#returns true inside an integration test
+		def integration_test?
+			defined?(post_via_redirect)
+		end
 end
